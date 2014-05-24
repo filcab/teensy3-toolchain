9c453ff2be425f63a6c13202d3a631b0d12ca593
Filipe Cabecinhas <filcab+github@gmail.com>
2014-05-24 13:48:46 -0700

    Added git submodule update to the fetch script

--- fetch.sh
+++ fetch.sh
@@ -6,7 +6,19 @@ ROOT=`pwd`
 popd > /dev/null
 
 DISTFILES="$ROOT/distfiles"
+SRCDIR="$ROOT/src"
 mkdir -p "$DISTFILES"
 
+# Deal with git submodules first
+echo Updating submodules
+if [ ! -d "$SRCDIR/pdclib" ]; then
+  git submodule init
+fi
+git submodule update
+
+# Verifying the download's signature is left as an exercise for the reader
+# The reader would also have to verify this repo, and there's no way they
+# can.
 BIN_FILE=binutils-2.24.tar.bz2
+echo Downloading $BIN_FILE
 curl -o "$DISTFILES/$BIN_FILE" "http://ftp.gnu.org/gnu/binutils/$BIN_FILE"
